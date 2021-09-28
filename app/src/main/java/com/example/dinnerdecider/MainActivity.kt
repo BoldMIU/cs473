22bfee83bde396030a2f3ac7b67aa5e62b2f8f49
Bold MIU <hi_u_baina@yahoo.com>
2021-09-28 18:09:25 -0500

    Added Empty string checker.

--- app/src/main/java/com/example/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/example/dinnerdecider/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     var list = ArrayList<String>(arrayListOf("Hamburger", "Pizza", "Mexican", "American", "Chinese"))
@@ -18,13 +19,21 @@ class MainActivity : AppCompatActivity() {
         var textView = findViewById<TextView>(R.id.foodexplore)
 
         textView.text = list.random()
+
     }
 
     fun add(view: android.view.View) {
         var editText = findViewById<EditText>(R.id.insert)
         var food = editText.text.toString()
-        list.add(food)
-        editText.text.clear()
+        if(editText.text.isEmpty()) {
+            Toast.makeText(applicationContext, "Please put food name", Toast.LENGTH_SHORT).show()
+        }
+
+        else {
+            list.add(food)
+            Toast.makeText(applicationContext, "Added!", Toast.LENGTH_SHORT).show()
+            editText.text.clear()
+            }
 
     }
 
